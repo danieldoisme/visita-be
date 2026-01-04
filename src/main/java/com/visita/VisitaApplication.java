0b4d3d46e8d4d1eebbf59a8a25c716364e2b8770
danielthanh <danieldoisme@gmail.com>
2026-01-04 21:38:45 +0700

    feat: enable Spring Data Web Support with DTO page serialization

--- src/main/java/com/visita/VisitaApplication.java
+++ src/main/java/com/visita/VisitaApplication.java
@@ -2,8 +2,11 @@ package com.visita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class VisitaApplication {
 
 	public static void main(String[] args) {
